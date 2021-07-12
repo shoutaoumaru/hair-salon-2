62fd8eb642755dc38b4d1b94ad5bf29482e4edc4
shoutaoumaru <otokmae.black@gmail.com>
2021-07-12 14:25:59 +0900

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(() => {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+  }, 3400);
+
+  const cb = function (el, inview) {
+    if (inview) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+    }
+  };
+  const so = new ScrollObserver('.clip-js', cb, {
+    rootMargin: '-100px 0px',
+  });
+
+  // const inviewAnimation = function (el, inview) {
+  //   if (inview) {
+  //     el.classList.add('inview');
+  //   } else {
+  //     el.classList.remove('inview');
+  //   }
+  // };
+  // const so2 = new ScrollObserver('.appear', inviewAnimation, {
+  //   rootMargin: '-130px 0px',
+  // });
+
+  const so2 = new ScrollObserver(
+    '.appear',
+    (el, inview) => {
+      if (inview) {
+        el.classList.add('inview');
+      } else {
+        el.classList.remove('inview');
+      }
+    }, {
+      rootMargin: '-100px 0px',
+    }
+  );
+  const so3 = new ScrollObserver(
+    '.cover-slide',
+    (el, inview) => {
+      if (inview) {
+        el.classList.add('inview');
+      } else {
+        el.classList.remove('inview');
+      }
+    }, {
+      rootMargin: '0px 0px',
+    }
+  );
+});
