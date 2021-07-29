f2f45c7742e9cb6e5661648d6a7d8c97bccde77d
Kanji <bukurabo@gmail.com>
2021-07-29 18:22:09 +0900

    c-footer 修正

--- includes/c-footer.php
+++ includes/c-footer.php
@@ -1,12 +1,9 @@
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/pace.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript">
-</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/animsition.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/libs/page.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/libs/scroll-btn.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/scripts/libs//mobile-menu.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/libs/scroll.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/scripts/libs/mobile-menu.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/libs/text-animation.js"></script>
 <?php wp_footer(); ?>
-
